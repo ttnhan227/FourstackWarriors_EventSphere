ea2de7a499c70bc63a1ccf9ce21422e2944edd79
Tran Trong Nhan <ttnhan227@gmail.com>
2025-09-10 14:59:04 +0700

    fix 404

--- src/main/java/fpt/aptech/eventsphere/controllers/HomeController.java
+++ src/main/java/fpt/aptech/eventsphere/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package fpt.aptech.eventsphere.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     
     @GetMapping("")
-    public String home() {
-        return "home/index";
+    public String home(Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            for (GrantedAuthority authority : authentication.getAuthorities()) {
+                if (authority.getAuthority().equals("ROLE_ADMIN")) {
+                    return "redirect:/admin/index";
+                } else if (authority.getAuthority().equals("ROLE_ORGANIZER")) {
+                    return "redirect:/organizer/index";
+                } else if (authority.getAuthority().equals("ROLE_PARTICIPANT")) {
+                    return "redirect:/participant/dashboard";
+                }
+            }
+        }
+        return "redirect:/auth/login";
     }
     
     @GetMapping("/generic")
